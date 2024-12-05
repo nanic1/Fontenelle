7cecb9975737cbde8126f3462e7a3cd9d2f46b7e
Pedro de Castro Kurtz <pedrokurtz.01@outlook.com>
2024-12-05 14:33:58 -0300

    carrosel da seção de depoimentos finalizado

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,43 @@
-alert("oi testando")
+var quantidade  = $('div#testemunhos ul').length
+var limite      = (quantidade * 336) * -1
+var largura     = limite + 336
+var view_largura= $('section#testemunha div#testemunha-centro').width()
+
+$('div#testemunhos').css('width', limite*-1)
+
+$('div#teste').html(limite)
+
+$('div#testemunha-direita').click( function() {
+
+    posicaoatual = $('div#testemunhos').position().left
+    novaposicao = posicaoatual - view_largura
+
+    if (limite < posicaoatual - view_largura) {
+        $('div#testemunhos').animate({left:novaposicao},1000)
+    }
+
+    if (limite > (novaposicao - view_largura)) {
+        $('div#testemunha-direita ion-icon').css('opacity', 0.5)
+        $('div#testemunha-direita ion-icon').css('cursor', 'not-allowed')
+    }
+
+    $('div#testemunha-esquerda ion-icon').css('opacity', 1)
+    $('div#testemunha-esquerda ion-icon').css('cursor', 'pointer')
+})
+
+$('div#testemunha-esquerda').click( function() {
+    posicaoatual = $('div#testemunhos').position().left
+
+    if (posicaoatual + view_largura <= 0) {
+        novaposicao = posicaoatual + view_largura
+        $('div#testemunhos').animate({left:novaposicao}, 1000)
+    }
+
+    if(novaposicao == 0) {
+        $('div#testemunha-esquerda ion-icon').css('opacity', 0.5)
+        $('div#testemunha-esquerda ion-icon').css('cursor', 'not-allowed')
+    }
+
+    $('div#testemunha-direita ion-icon').css('opacity', 1)
+    $('div#testemunha-direita ion-icon').css('cursor', 'pointer')
+})
