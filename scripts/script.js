f1282ad553323f55a05dc7dc9b82b606369e6b75
Pedro de Castro Kurtz <pedrokurtz.01@outlook.com>
2024-12-09 15:22:33 -0300

    animações e transições finalizadas

--- scripts/script.js
+++ scripts/script.js
@@ -64,3 +64,126 @@ $("ion-icon#instagram").click(function() {
 $("ion-icon#linkedin").click(function() {
     window.open("https://www.linkedin.com/in/pedrocastrokurtz/")
 })
+
+// CÓDIGOS DE ANIMAÇÃO
+
+$('div#testemunhos ul:eq(0)').css('opacity', 0)
+$('div#testemunhos ul:eq(0)').css('opacity', 1)
+$('div#testemunhos ul:eq(0)').css('opacity', 2)
+
+$('section#domingo-perfeito').waypoint( function(direcao) {
+    if ( direcao == 'down') {
+        $('div.folha1').removeClass('animate__animated animate__fadeOutUp')
+        $('div.folha3').removeClass('animate__animated animate__fadeOutUp')
+        $('div.camarao').removeClass('animate__animated animate__fadeOutUp animate__slow')
+        $('div.tomate').removeClass('animate__animated animate__fadeOutUp animate__slow')
+        $('div.queijo').removeClass('animate__animated animate__fadeOutUp animate_slower')
+        $('div.folha2').removeClass('animate__animated animate__fadeOutUp animate_slower')
+
+        $('div.folha1').addClass('animate__animated animate__fadeInUp')
+        $('div.folha3').addClass('animate__animated animate__fadeInUp')
+        $('div.camarao').addClass('animate__animated animate__fadeInUp animate__slow')
+        $('div.tomate').addClass('animate__animated animate__fadeInUp animate__slow')
+        $('div.queijo').addClass('animate__animated animate__fadeInUp animate_slower')
+        $('div.folha2').addClass('animate__animated animate__fadeInUp animate_slower')
+    }
+
+    if ( direcao == "up") {
+        $('div.folha1').addClass('animate__animated animate__fadeOutUp')
+        $('div.folha3').addClass('animate__animated animate__fadeOutUp')
+        $('div.camarao').addClass('animate__animated animate__fadeOutUp animate__slow')
+        $('div.tomate').addClass('animate__animated animate__fadeOutUp animate__slow')
+        $('div.queijo').addClass('animate__animated animate__fadeOutUp animate_slower')
+        $('div.folha2').addClass('animate__animated animate__fadeOutUp animate_slower')
+    }
+
+}, {
+    offset:'450px;'
+})
+
+
+$('section#pizza').waypoint( function(direcao) {
+    if (direcao == 'down') {
+        $('section#pizza p').removeClass('animate__animated animate__fadeOutUp')
+        $('section#pizza div.pizza-opcoes').removeClass('animate__animated animate__fadeOutUp animate_slow')
+
+        $('section#pizza p').addClass('animate__animated animate__fadeInUp')
+        $('section#pizza div.pizza-opcoes').addClass('animate__animated animate__fadeInUp animate_slow')
+    }
+    if (direcao == 'up') {
+        $('section#pizza p').addClass('animate__animated animate__fadeOutUp')
+        $('section#pizza div.pizza-opcoes').addClass('animate__animated animate__fadeOutUp animate_slow')
+    }
+}, {
+    offset: '450px;'
+})
+
+$('section#testemunha').waypoint( function(direcao) {
+    if (direcao == 'down') {
+        $('div#testemunhos ul:eq(0)').removeClass('animate__animated animate__fadeOutUp animate__slower')
+        $('div#testemunhos ul:eq(1)').removeClass('animate__animated animate__fadeOutUp animate__slow')
+        $('div#testemunhos ul:eq(2)').removeClass('animate__animated animate__fadeOutUp animate__slower')
+
+        $('div#testemunhos ul:eq(0)').addClass('animate__animated animate__fadeInUp animate__slower')
+        $('div#testemunhos ul:eq(1)').addClass('animate__animated animate__fadeInUp animate__slow')
+        $('div#testemunhos ul:eq(2)').addClass('animate__animated animate__fadeInUp animate__slower')
+    }
+    if (direcao == 'up') {
+        $('div#testemunhos ul:eq(0)').addClass('animate__animated animate__fadeOutUp animate__slower')
+        $('div#testemunhos ul:eq(1)').addClass('animate__animated animate__fadeOutUp animate__slow')
+        $('div#testemunhos ul:eq(2)').addClass('animate__animated animate__fadeOutUp animate__slower')
+    }
+}, {
+    offset: '400px;'
+})
+
+$('div#transicao-testemunha-plano').waypoint( function(direcao) {
+    if (direcao == "down") {
+        $('div#decoracao1').removeClass('animate__animated animate__fadeOutLeft animate__slow')
+        $('div#decoracao2').removeClass('animate__animated animate__fadeOut animate__slow')
+        $('div#decoracao3').removeClass('animate__animated animate__fadeOutRight animate__slow')
+
+        $('div#decoracao1').addClass('animate__animated animate__fadeInLeft animate__slow')
+        $('div#decoracao2').addClass('animate__animated animate__fadeIn animate__slow')
+        $('div#decoracao3').addClass('animate__animated animate__fadeInRight animate__slow')
+    }
+    if (direcao == 'up') {
+        $('div#decoracao1').addClass('animate__animated animate__fadeOutLeft animate__slow')
+        $('div#decoracao2').addClass('animate__animated animate__fadeOut animate__slow')
+        $('div#decoracao3').addClass('animate__animated animate__fadeOutRight animate__slow')
+    }
+}, {
+    offset: '800px;'
+})
+
+$('section#plano-principal').waypoint( function(direcao){
+    if (direcao == 'down') {
+        $('div.planos:eq(0)').removeClass('animate__animated animate__fadeOutLeft animate__slower')
+        $('div.planos:eq(1)').removeClass('animate__animated animate__fadeOutUp animate__slow')
+        $('div.planos:eq(2)').removeClass('animate__animated animate__fadeOutRight animate__slower')
+
+        $('div.planos:eq(0)').addClass('animate__animated animate__fadeInLeft animate__slower')
+        $('div.planos:eq(1)').addClass('animate__animated animate__fadeInUp animate__slow')
+        $('div.planos:eq(2)').addClass('animate__animated animate__fadeInRight animate__slower')
+    }
+    if (direcao == 'up') {
+        $('div.planos:eq(0)').addClass('animate__animated animate__fadeOutLeft animate__slower')
+        $('div.planos:eq(1)').addClass('animate__animated animate__fadeOutUp animate__slow')
+        $('div.planos:eq(2)').addClass('animate__animated animate__fadeOutRight animate__slower')
+    }
+}, {
+    offset: '450px;'
+})
+
+$('footer').waypoint( function(direcao) {
+    if (direcao == 'down') {
+        $('div#info').removeClass('animate__animated animate__fadeOut animate__slow')
+        
+        $('div#info').addClass('animate__animated animate__fadeIn animate__slow')
+    }
+    if (direcao == 'up') {
+        $('div#info').addClass('animate__animated animate__fadeOut animate__slow')
+    }
+}, {
+    offset: '650px;'
+})
