b1df3ec407517d53d075308f2d42f3d430de699f
Pedro de Castro Kurtz <131386211+nanic1@users.noreply.github.com>
2026-06-29 10:37:04 -0300

    Update LinkedIn URL in script.js

--- scripts/script.js
+++ scripts/script.js
@@ -62,7 +62,7 @@ $("ion-icon#instagram").click(function() {
 
 //Linkedin
 $("ion-icon#linkedin").click(function() {
-    window.open("https://www.linkedin.com/in/pedro-de-castro-kurtz-03b566269/")
+    window.open("https://www.linkedin.com/in/pedro-de-castro-kurtz/")
 })
 
 // CÓDIGOS DE ANIMAÇÃO
